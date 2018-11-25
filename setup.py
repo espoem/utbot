5bc8acb83a62e4b2bf7d0f990a3e7b890231e3a0
Martin Šmíd <martin.smid94@seznam.cz>
2018-11-25 16:14:03 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+def load_description():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'README.md')
+    with open(filename, 'r') as f:
+        return f.read()
+
+requirements = ['beem', 'discord-webhook']
+
+setup(
+    name="utbot",
+    version="0.0.1",
+    author="Martin Šmíd",
+    author_email="martin.smid94@seznam.cz",
+    packages=find_packages(),
+    long_description=load_description(),
+    long_description_type="text/markdown",
+    url="https://github.com/espoem/utbot",
+    install_requires=requirements,
+)
